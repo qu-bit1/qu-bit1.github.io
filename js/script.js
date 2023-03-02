4b7bace56fe5bcdc1871a63daf5d90b9f4b0b90b
qu-bit1 <103852686+qu-bit1@users.noreply.github.com>
2023-03-02 18:22:08 +0530

    removed scrollReveal for sub-services

--- js/script.js
+++ js/script.js
@@ -26,5 +26,5 @@ const sr = ScrollReveal ({
 sr.reveal('.home-text',{delay:350, origin:'left'})
 sr.reveal('.home-img',{delay:350, origin:'right'})
 
-sr.reveal('.sub-service,.about,.donut,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about,.donut,.contact',{delay:200, origin:'bottom'})
 
